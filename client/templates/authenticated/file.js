045af1e704852150152b2babb65ceedf9831027b
Aviv Hatzir <avivhatzir@gmail.com>
2015-11-03 11:33:19 +0200

    broken connect modal

--- client/templates/authenticated/file.js
+++
@@ -1,9 +0,0 @@
-/**
- * Created by avivhatzir on 26/10/2015.
- */
-Template.file.helpers({
-  //isImage( url ) {
-  //  const formats = [ 'jpg', 'jpeg', 'png', 'gif' ];
-  //  return _.find( formats, ( format ) => url.indexOf( format ) > -1 );
-  //}
-});
