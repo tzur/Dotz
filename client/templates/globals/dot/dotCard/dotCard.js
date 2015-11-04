27697031de4128158b5d973f2352687ee1b9acc4
otniel <otniel.levi@gmail.com>
2015-11-04 15:04:39 +0200

    few changes

--- client/templates/globals/dot/dotCard/dotCard.js
+++ client/templates/globals/dot/dotCard/dotCard.js
@@ -1,30 +1,22 @@
 _data = {};
 
 Template.dotCard.onCreated(function() {
-  //console.log("%%%%%%%%%%%%%%%%%%% this._id is 00000 " + this._id);
   let self = this;
   self.autorun(function() {
 
     if (_data.smartRef) {
-      //self.subscribe('dotShow', _data.smartRef.dotId);
-      console.log("%%%%%%%%%%%%%%%%%%% on created _data.smartRef.dotId is " + _data.smartRef.dotId);
-
-      let dot = Dotz.findOne(_data.smartRef.dotId);
-      if (dot) {
-        console.log("%%%%%%%%%%%%%%%%%%% on created dot is " + dot);
+      let dotId = _data.smartRef.dotId;
+      if (dotId) {
+        self.subscribe('dotShow', _data.smartRef.dotId);
+        _data.dot = Dotz.findOne(dotId);
+        if (_data.dot) {
+        }
       }
 
     }
     let userId = this.ownerUserId;
     if (userId) {
       self.subscribe('user', userId);
-      console.log("%%%%%%%%%%%%%%%%%%% on created userId is " + userId);
-    }
-
-    _data.test = this.title;
-    if (_data.test) {
-      console.log("%%%%%%%%%%%%%%%%%%% on created this.title is " + this.title);
-      console.log("%%%%%%%%%%%%%%%%%%% on created _data.test is " + _data.test);
     }
 
     let user = Meteor.users.findOne(userId);
@@ -34,68 +26,10 @@ Template.dotCard.onCreated(function() {
 });
 
 
-/*
-
-
- Template.dotCard.onCreated(function() {
- //console.log("%%%%%%%%%%%%%%%%%%% this._id is 00000 " + this._id);
- let self = this;
- self.autorun(function() {
-
-
- if (_data.smartRef) {
- self.subscribe('dotShow', _data.smartRef.dotId);
- console.log("%%%%%%%%%%%%%%%%%%% on created _data.smartRef.dotId is " + _data.smartRef.dotId);
-
- let dot = Dotz.findOne(_data.smartRef.dotId);
- if (dot) {
- console.log("%%%%%%%%%%%%%%%%%%% on created dot is " + dot);
- }
-
- }
- let userId = this.ownerUserId;
- if (userId) {
- self.subscribe('user', userId);
- console.log("%%%%%%%%%%%%%%%%%%% on created userId is " + userId);
- }
-
- _data.test = this.title;
- if (_data.test) {
- console.log("%%%%%%%%%%%%%%%%%%% on created this.title is " + this.title);
- console.log("%%%%%%%%%%%%%%%%%%% on created _data.test is " + _data.test);
- }
-
- let user = Meteor.users.findOne(userId);
- _data.user = user;
-
- });
- });
-
- */
-//Template.dotCard.onRendered (function(){
-//  let self = this;
-//  self.autorun(function() {
-//  console.log("%%%%%%%%%%%%%%%%%%% on onRendered this.title is " + _data.dot.title);
-//  });
-//});
-
-
-//Template.dotCard.onRendered (function(){
-//  $(".limitP-mobile").each(function(i){
-//    len=$(this).text().length;
-//    if(len>200)
-//    {
-//      $(this).text($(this).text().substr(0,200)+'...');
-//    }
-//  });
-//});
 
 Template.dotCard.helpers({
   dotData: function() {
-    _data.smartRef = this;
-    _data.dot = this;
-    _data.test = this.title;
-    return _data;
+    return this;
   },
   //userName: function(){
   //  return this.owner.userName;
@@ -115,7 +49,7 @@ Template.dotCard.helpers({
     return (moment(this.createdAt).fromNow())
   },
   eventDate: function(){
-    return ( moment(_data.dot.startDateAndHour).fromNow());
+    return ( moment(_data.dotShow.startDateAndHour).fromNow());
   },
   showOriginalDotCard: function() {
     let parentData = Template.parentData();
@@ -270,7 +204,7 @@ Template.dotCard.events({
    */
 
   'click .connect': function(){
-
+console.log ("this is " + this._id);
     Modal.show('connectDotModal',{
       data:{
         dotId: this._id
