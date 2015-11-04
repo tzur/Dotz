c3cb7b00fe95c72e7db3759a8f4994d5d7c8d3d2
zur Tene <zurtene@gmail.com>
2015-11-04 15:38:47 +0200

    merge otni atgain

--- both/modules/follow.js
+++ both/modules/follow.js
@@ -1,6 +1,7 @@
-let _followUser =(followingUserId, followedUserId) ->{
+let _followUser = function(followingUserId, followedUserId){
   Meteor.call('followUser', followingUserId, followedUserId)
-
 };
-let _unFollowUser = ()
+let _unFollowUser =  function(unFollowUserId, followedUserId){
+  Meteor.call('unFollowUser', unFollowUserId, followedUserId);
+};
 Modules.both.Dotz.followUser = _followUser;
