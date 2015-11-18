414017e6d37c891c352a571c5d8bf5ecdc2f069b
zur Tene <zurtene@gmail.com>
2015-11-18 11:13:52 +0200

    fixed tests

--- client/templates/globals/search/dotCardSearch.js
+++ client/templates/globals/search/dotCardSearch.js
@@ -1,21 +1,8 @@
-//Template.dotCardSearch.helpers({
-//  isMyDot: function(){
-//    return Meteor.userId() === this.dot.ownerUserId;
-//  }
-//});
-//Template.dotCardSearch.events({
-//  'click .toUser': function(){
-//    Router.go('/user/' + this.owner.userId);
-//  },
-//
-//  'click .connect': function(){
-//    Modal.show('connectDotModal',{
-//      data:{
-//        dotId: this.dot._id
-//      }
-//    });
-//  }
-//});
+
+Template.dotCardSearch.onCreated(function(){
+  console.log(this);
+});
+
 
 
 
