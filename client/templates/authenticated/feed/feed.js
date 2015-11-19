1dcb07c48bed36b069a55959aac6a2397a9f7aef
zur Tene <zurtene@gmail.com>
2015-11-19 17:26:16 +0200

    feed reverse fixed

--- client/templates/authenticated/feed/feed.js
+++ client/templates/authenticated/feed/feed.js
@@ -4,7 +4,15 @@ Template.feed.onCreated(function(){
 Template.feed.helpers({
   feedDotz: function(){
     if (Meteor.user().profile.feedDotz){
-      return Meteor.user().profile.feedDotz.reverse();
+      //Reverse the array in the fastest way according to stackOverFlow:
+      let tempArray = Meteor.user().profile.feedDotz;
+      let oppositeArray = [];
+      let i = tempArray.length -1;
+      while(i > -1){
+        oppositeArray.push(tempArray[i]);
+        i--;
+      }
+      return oppositeArray;
     }
   }
 });
