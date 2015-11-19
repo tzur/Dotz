013fecc608e4b0af1525abff9670a58642473441
Aviv Hatzir <avivhatzir@gmail.com>
2015-11-19 11:13:39 +0200

    mm

--- client/templates/authenticated/feed/feed.js
+++ client/templates/authenticated/feed/feed.js
@@ -1,21 +1,9 @@
-_data = {};
 Template.feed.onCreated(function(){
-  var self = this;
-  self.autorun(function(){
-    if(Meteor.userId()){
-      self.subscribe('user', Meteor.userId());
-      if (Meteor.user().profile.feedDotz){
-        self.subscribe('smartRefToUsersCursor', Meteor.user().profile.feedDotz);
-        self.subscribe('smartRefToDotzCursor', Meteor.user().profile.feedDotz);
-      }
-    }
-   _data.user = Meteor.users.findOne(Meteor.userId());
-  });
 });
 Template.feed.helpers({
   feedDotz: function(){
-    if (_data.user.profile.feedDotz){
-      return Modules.both.Dotz.smartRefToDataObject(_data.user.profile.feedDotz);
+    if (Meteor.user().profile.feedDotz){
+      return Meteor.user().profile.feedDotz;
     }
 
   }
