e8b00ca18f027fbaf4b729743f3cc990510995a9
Aviv Hatzir <avivhatzir@gmail.com>
2015-11-23 13:02:38 +0200

    mm

--- client/templates/authenticated/feed/findUsersToFollowModal.js
+++ client/templates/authenticated/feed/findUsersToFollowModal.js
@@ -10,12 +10,16 @@ Template.findUsersToFollowModal.onCreated(function(){
   });
   if(Meteor.user().profile.userSlug != "dotz"){
     let dotz = Meteor.users.findOne({"profile.userSlug": "dotz"});
-    Modules.both.Dotz.followUser(Meteor.userId(), dotz._id)
+    if(dotz){
+      Modules.both.Dotz.followUser(Meteor.userId(), dotz._id)
+    }
   }
 
   if(Meteor.user().profile.userSlug != "telavivglobal"){
     let telAvivGlobal = Meteor.users.findOne({"profile.userSlug": "telavivglobal"});
-    Modules.both.Dotz.followUser(Meteor.userId(), telAvivGlobal._id)
+    if(telAvivGlobal){
+      Modules.both.Dotz.followUser(Meteor.userId(), telAvivGlobal._id)
+    }
   }
 
 });
@@ -23,13 +27,15 @@ Template.findUsersToFollowModal.onCreated(function(){
 Template.findUsersToFollowModal.helpers({
   usersList: function(){
     let usersIdArray = Tools.findOne({name: "featuredUsers"});
-    if(usersArray){
-      let usersArray =  Meteor.users.find({$and: [{_id: {$in: usersArray.featuredUsers }}, {_id: {$ne: Meteor.userId()}} ] })
+    if(usersIdArray){
+      let usersArray =  Meteor.users.find({$and: [{_id: {$in: usersIdArray.featuredUsers }}, {_id: {$ne: Meteor.userId()}} ] })
       if(usersArray){
         return usersArray;
       }
     }
-  }
+  },
+
+
 });
 
 Template.findUsersToFollowModal.events({
