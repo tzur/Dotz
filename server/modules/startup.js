8bd5862e64b2d1b6f85dd7e64cdb5d51f6c8e7ea
zur Tene <zurtene@gmail.com>
2015-11-25 11:45:24 +0200

    kadira configured

--- server/modules/startup.js
+++ server/modules/startup.js
@@ -6,5 +6,5 @@ let startup = () => {
 var _setBrowserPolicies = () => {};
 
 var _generateAccounts = () => Modules.server.generateAccounts();
-
+//Kadira.connect('WhNBWk69WyNe4e6SH', '97449698-c726-4fb0-9a28-704083cd9335');
 Modules.server.startup = startup;
