8a5408cbeeb143120ea737668820def0d4a6f4f2
otniel <otniel.levi@gmail.com>
2015-11-30 21:21:21 +0200

    new footer

--- client/templates/layouts/default.js
+++ client/templates/layouts/default.js
@@ -1,9 +1,23 @@
 
+Template.default.onRendered(function() {
+  if ( !Meteor.userId() && Session.get('joinUsDivOn') ) {
+      $(window).scroll(function() {
+          if (document.body.scrollTop > 400) {
+            Session.set('footerIsWelcome', true)
+          }
+          else {
+            Session.set('footerIsWelcome', false)
+          }
+      });
+  }
+});
+
+
 Template.default.helpers({
 
   joinUsDivOn: function() {
-    if ( !Meteor.user() && Session.get('joinUsDivOn') ) {
-      return true;
+    if ( Session.get('joinUsDivOn') ) {
+      return Session.get('footerIsWelcome');
     }
   }
 
--- client/templates/layouts/footer/footer.js
+++ client/templates/layouts/footer/footer.js
@@ -1,8 +1,18 @@
 
+Template.footer.onDestroyed(function(){
+
+  //This sessions also destroyed by the event-listeners (below):
+  Session.set('joinUsDivOn', false);
+  Session.set('iAmAHotel', false); //TBD
+  Session.set('footerIsWelcome', false);
+
+});
+
+
 Template.footer.helpers({
 
   iAmAHotel: function() {
-    if ( !Meteor.user() && Session.get('iAmAHotel') ) {
+    if ( !Meteor.userId() && Session.get('iAmAHotel') ) {
       return true;
     }
   }
@@ -15,17 +25,20 @@ Template.footer.events({
   'click #_signUpBtn': function(){
     Session.set('joinUsDivOn', false);
     Session.set('iAmAHotel', false); //TBD
+    Session.set('footerIsWelcome', false);
   },
 
   'click #_loginBtn': function(){
     Session.set('joinUsDivOn', false);
     Session.set('iAmAHotel', false); //TBD
+    Session.set('footerIsWelcome', false);
   },
 
   //TBD:
   'click #_signInExitBtn': function(){
     Session.set('joinUsDivOn', false);
     Session.set('iAmAHotel', false); //TBD
+    Session.set('footerIsWelcome', false);
   }
 
 });
