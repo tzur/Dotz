488a9e6b3331cd1ad1915df17d526ebedb1d92e6
otniel <otniel.levi@gmail.com>
2015-12-28 11:12:46 +0200

    mc

--- client/templates/globals/user/userShow.js
+++ client/templates/globals/user/userShow.js
@@ -70,16 +70,6 @@ Template.userShow.onRendered(function() {
 
   fbAsyncInit();
 
-  //Try to embed FB:
-//
-//(function(d, s, id) {
-//      var js, fjs = d.getElementsByTagName(s)[0];
-//      if (d.getElementById(id)) return;
-//      js = d.createElement(s); js.id = id;
-//      js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.5&appId=937922069633455";
-//      fjs.parentNode.insertBefore(js, fjs);
-//    }(document, 'script', 'facebook-jssdk'));
-
 });
 
 
