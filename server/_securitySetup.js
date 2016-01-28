97a81ce3737371d943d8c87d0cba66b6e4a646f3
otniel <otniel.levi@gmail.com>
2016-01-28 10:21:32 +0200

    _security browser policy

--- server/_securitySetup.js
+++ server/_securitySetup.js
@@ -1,13 +1,20 @@
 //browser policy setup
 
+BrowserPolicy.framing.allowAll();
+
+//Try - CSE:
+BrowserPolicy.content.allowDataUrlForAll("http://cse.google.com");
+BrowserPolicy.content.allowDataUrlForAll("https://cse.google.com");
+BrowserPolicy.content.allowDataUrlForAll("http://*.google.com");
+BrowserPolicy.content.allowDataUrlForAll("https://*.google.com");
+BrowserPolicy.content.allowDataUrlForAll("http://*.google.com/*");
+BrowserPolicy.content.allowDataUrlForAll("https://*.google.com/*");
+
 //See more on http://content-security-policy.com :
 BrowserPolicy.content.allowOriginForAll("data:"); //TBD
-//BrowserPolicy.content.allowOriginForAll("unsafe-eval");
-BrowserPolicy.content.allowOriginForAll("unsafe-inline");
 
-//All the images (included .ico) are allow - TBD:
+//script-src
 BrowserPolicy.content.allowImageOrigin("*");
-//BrowserPolicy.content.allowOriginForAll("*");
 
 //Google:
 BrowserPolicy.content.allowEval("https://maps.googleapis.com");
@@ -18,9 +25,9 @@ BrowserPolicy.content.allowOriginForAll("https://fonts.gstatic.com");
 BrowserPolicy.content.allowOriginForAll("http://*.googletagmanager.com");
 BrowserPolicy.content.allowOriginForAll("http://*.google-analytics.com");
 BrowserPolicy.content.allowOriginForAll("https://*.google-analytics.com");
-BrowserPolicy.content.allowOriginForAll('https://lookup-id.com/');
-BrowserPolicy.content.allowOriginForAll('https://lookup-id.com/*');
+//BrowserPolicy.content.allowOriginForAll('https://lookup-id.com/');
 
+//Google CSE:
 BrowserPolicy.content.allowEval("https://*.googleapis.com");
 BrowserPolicy.content.allowEval('*.cse.google.com/*');
 BrowserPolicy.content.allowEval('http://cse.google.com/*');
@@ -32,11 +39,8 @@ BrowserPolicy.content.allowInlineScripts('http://cse.google.com/*');
 BrowserPolicy.content.allowInlineScripts('*.cse.google.com/*');
 BrowserPolicy.content.allowDataUrlForAll('*.cse.google.com/*');
 BrowserPolicy.content.allowDataUrlForAll('http://cse.google.com/*');
-BrowserPolicy.framing.allowAll();
-BrowserPolicy.content.allowInlineStyles();
-BrowserPolicy.content.allowDataUrlForAll();
-BrowserPolicy.content.allowInlineScripts();
 BrowserPolicy.content.allowInlineScripts("https://*.googleapis.com");
+
 //Analytics:
 BrowserPolicy.content.allowOriginForAll("http://*.segment.com");
 BrowserPolicy.content.allowOriginForAll("cdn.mxpnl.com");
@@ -93,17 +97,17 @@ BrowserPolicy.content.allowOriginForAll("http://platform.twitter.com/*");
 
 //Temp - tbd:
 
-BrowserPolicy.content.allowOriginForAll("http://*.giphy.com");
+//BrowserPolicy.content.allowOriginForAll("http://*.giphy.com");
 BrowserPolicy.content.allowOriginForAll("http://i.giphy.com/YwTXexxX2yEVy.gif");
 
-BrowserPolicy.content.allowImageOrigin("http://s2.postimg.org/6yq2vojd1/Location_icon_lg.png");
-BrowserPolicy.content.allowImageOrigin("http://s2.postimg.org/6yq2vojd1/location_icon_lg.png");
-BrowserPolicy.content.allowImageOrigin("http://s2.postimg.org/6yq2vojd1/Location_icon_lg.png");
-BrowserPolicy.content.allowOriginForAll("http://s2.postimg.org/6yq2vojd1/Location_icon_lg.png");
-BrowserPolicy.content.allowOriginForAll("http://*.postimg.org");
+//BrowserPolicy.content.allowImageOrigin("http://s2.postimg.org/6yq2vojd1/Location_icon_lg.png");
+//BrowserPolicy.content.allowImageOrigin("http://s2.postimg.org/6yq2vojd1/location_icon_lg.png");
+//BrowserPolicy.content.allowImageOrigin("http://s2.postimg.org/6yq2vojd1/Location_icon_lg.png");
+//BrowserPolicy.content.allowOriginForAll("http://s2.postimg.org/6yq2vojd1/Location_icon_lg.png");
+//BrowserPolicy.content.allowOriginForAll("http://*.postimg.org");
 BrowserPolicy.content.allowImageOrigin("https://dotz-dev-images.s3.amazonaws.com/otni/lights-night-bokeh-city.jpg");
-BrowserPolicy.content.allowConnectOrigin("ws://localhost:5000");
-BrowserPolicy.content.allowConnectOrigin("http://localhost:5000");
+//BrowserPolicy.content.allowConnectOrigin("ws://localhost:5000");
+//BrowserPolicy.content.allowConnectOrigin("http://localhost:5000");
 //BrowserPolicy.framing.allowAll();
 BrowserPolicy.content.allowOriginForAll("https://player.vimeo.com");
 BrowserPolicy.content.allowOriginForAll("https://*.gstatic.com");
