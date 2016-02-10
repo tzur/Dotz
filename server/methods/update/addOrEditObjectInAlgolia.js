a555464ff51e8c3df375378a78a7ba59173126a6
otniel <otniel.levi@gmail.com>
2016-02-10 22:41:13 +0200

    fix algolia + (server side)

--- server/methods/update/addOrEditObjectInAlgolia.js
+++ server/methods/update/addOrEditObjectInAlgolia.js
@@ -1,93 +1,105 @@
-/**
- * Created by avivhatzir on 17/11/2015.
- */
+
 Meteor.methods({
+
   addOrEditObjectInAlgolia(docSlug, isUser){
     check(docSlug, String);
     check(isUser, Boolean);
-    if(process.env.NODE_ENV === "production"){
-      console.log("I am in (Algolia check): " + process.env.NODE_ENV);
 
-      let array;
-      let docIndex;
-      check(docSlug, String);
-      check(isUser, Boolean);
-      var client = AlgoliaSearch("WB8PQ4YYUT", "aa84f19d874aeb14d8bfa4d0d4b73b4d");
+    if (process.env.NODE_ENV === "production") {
+        console.log("I am in (Algolia check): " + process.env.NODE_ENV);
+        let array;
+        let docIndex;
+        var client = AlgoliaSearch("WB8PQ4YYUT", "d1d8771f079e6676097bded60b6c8355");
 
-      //The old one
+        if (isUser) {
+          //TBD:
+          let currentDoc = Meteor.users.findOne({"profile.userSlug": docSlug});
+          currentDoc.objectID = currentDoc._id;
+          array = [{"objectID": currentDoc.objectID,"username": currentDoc.username, "profile": currentDoc.profile, "_id": currentDoc._id}];
+          docIndex = "users_DOTZ";
 
-      //var client = AlgoliaSearch("OE5LQTXY83", "bd14aab9d22ce75c25d286f9821b89c3");
+        // if is dot/list:
+        } else {
+          let currentDoc = Dotz.findOne({dotSlug: docSlug});
+          currentDoc.objectID = currentDoc._id;
+          if (currentDoc.dotType === "Dot") {
+            docIndex = currentDoc.dotSubType.toLowerCase() + "s_DOTZ";
+          } else if (currentDoc.dotType === "List") {
+            docIndex = "lists_DOTZ";
+          }
+          //currentDoc.inDotz = currentDoc.inDotz.length + currentDoc.totalUpvotes.length;
 
-      if (isUser){
-        let currentDoc = Meteor.users.findOne({"profile.userSlug": docSlug});
-        currentDoc.objectID = currentDoc._id;
-
-        array = [{"objectID": currentDoc.objectID,"username": currentDoc.username, "profile": currentDoc.profile, "_id": currentDoc._id}]
-        docIndex = currentDoc.roles.firstGroup[0] + "Users"
-
-      }
-      else{
-
-        let currentDoc = Dotz.findOne({dotSlug: docSlug});
-        currentDoc.objectID = currentDoc._id;
-        if(currentDoc.dotType === "Dot"){
-          docIndex = currentDoc.category[0] + "Dotz"
+          if (currentDoc.location && currentDoc.location.latLng) {
+            currentDoc._geoloc = {"lat":currentDoc.location.latLng[0], "lng":currentDoc.location.latLng[1]};
+          }
+          array = [currentDoc];
         }
-        else{
-          docIndex = currentDoc.category[0] + "Lists"
-        }
-        currentDoc.inDotz = currentDoc.inDotz.length + currentDoc.totalUpvotes.length;
-        if(currentDoc.location && currentDoc.location.latLng){
-          currentDoc._geoloc = {"lat":currentDoc.location.latLng[0], "lng":currentDoc.location.latLng[1]};
-        }
-        array = [currentDoc];
-      }
 
+        var index = client.initIndex(docIndex);
 
+        // array contains the data you want to save in the index
+        index.saveObjects(array, function (error, content) {
+          if (error) {
+            console.error('Error:', error);
+          } else {
+            //console.log('Content:', content);
+          }
+        });
 
-      var index = client.initIndex(docIndex);
-
-// array contains the data you want to save in the index
-
-      index.saveObjects(array, function (error, content) {
-        if (error) console.error('Error:', error);
-        else console.log('Content:', content);
-      });
-    }
-    else{
-      console.log("im in: " + process.env.NODE_ENV);
-      return false
+    //In development env. :
+    } else {
+        console.log("im in: " + process.env.NODE_ENV);
+        return false;
     }
 
   },
 
-  deleteDotzFromAlgolia(dotId, dotType){
+  deleteDotzFromAlgolia(dotId, dotType, dotSubType, isUser, userId){
     check(dotId, String);
     check(dotType, String);
-    if(process.env.NODE_ENV === "production" && Meteor.isServer) {
+    check(dotSubType, String);
+    //check(isUser, String);
+    //check(userId, String);
 
-      var client = AlgoliaSearch("WB8PQ4YYUT", "aa84f19d874aeb14d8bfa4d0d4b73b4d");
-      //The old one
-      //var client = AlgoliaSearch("OE5LQTXY83", "bd14aab9d22ce75c25d286f9821b89c3");
+    if (process.env.NODE_ENV === "production" && Meteor.isServer) {
 
-      let objectIndex;
-      let dot = Dotz.findOne("dotId");
-      let index;
+    //console.log("delete dotId from algolia : " + dotId)
 
-      if (dotType === "Dot") {
-        index = client.initIndex(dot.category[0] + "Dotz");
-      }
-      else {
-        index = client.initIndex(dot.category[0] + "Lists");
-      }
+        var client = AlgoliaSearch("WB8PQ4YYUT", "d1d8771f079e6676097bded60b6c8355");
+        //let objectIndex;
+        //let dot = Dotz.findOne("dotId");
+        let index;
 
+        //if (dotType === "Dot") {
+        //  index = client.initIndex(dot.category[0] + "Dotz");
+        //}
+        //else {
+        //  index = client.initIndex(dot.category[0] + "Lists");
+        //}
 
-// array contains the data you want to save in the index
+        if (dotType === "Dot") {
+          index = client.initIndex(dotSubType.toLowerCase() + "s_DOTZ");
+        } else if (dotType === "List") {
+          index = client.initIndex("lists_DOTZ");
+        }
 
-      index.deleteObject(dotId, function (error, content) {
-        if (error) console.error('Error:', error);
-        else console.log('Content:', content);
-      });
+        // delete the record with objectID="dotId":
+        index.deleteObject(dotId, function(err) {
+          if (err) {
+            console.error('Error: dotId: ' + dotId + ', (on deleteDotzFromAlgolia): ' + err);
+            return;
+          }
+        });
+
+        //index.deleteObject(dotId, function (error, content) {
+        //  if (error) {
+        //    console.error('Error (on deleteDotzFromAlgolia): ', error);
+        //  } else {
+        //    //console.log('Content:', content);
+        //  }
+        //});
+
     }
   }
+
 });
