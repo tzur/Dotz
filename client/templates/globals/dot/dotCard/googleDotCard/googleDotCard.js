c09f388a3ef41131f7fed6e35fe64fe8c9a743dc
otniel <otniel.levi@gmail.com>
2016-02-23 16:35:46 +0200

    facebook links :) 2

--- client/templates/globals/dot/dotCard/googleDotCard/googleDotCard.js
+++ client/templates/globals/dot/dotCard/googleDotCard/googleDotCard.js
@@ -8,23 +8,23 @@ Template.googleDotCard.helpers({
 Template.googleDotCard.events({
   'click ._connectGoogle': function(){
 
-    Session.set('link', "active");
-    Session.set('selectedType', "Link");
-
-    Session.set('dotFromGoogleCard', this.googleDot.googleLinkUrl);
-
-    Modal.show('createNewDot_Modal',{
-        initialDataForFormFields: {
-          //TODO >>> implement embedly here, so we could to "crawl" the full text:
-          title: this.googleDot.googleTitle,
-          description: this.googleDot.googleDescription,
-          img: this.googleDot.googleImg,
-          linkName: this.googleDot.googleWebsiteName,
-          //
-          linkUrl: this.googleDot.googleLinkUrl
-        },
-        parentDotId: this.parentDotId
-
-    });
+    //Session.set('link', "active");
+    //Session.set('selectedType', "Link");
+    //
+    //Session.set('dotFromGoogleCard', this.googleDot.googleLinkUrl);
+    //
+    //Modal.show('createNewDot_Modal',{
+    //    initialDataForFormFields: {
+    //      //TODO >>> implement embedly here, so we could to "crawl" the full text:
+    //      title: this.googleDot.googleTitle,
+    //      description: this.googleDot.googleDescription,
+    //      img: this.googleDot.googleImg,
+    //      linkName: this.googleDot.googleWebsiteName,
+    //      //
+    //      linkUrl: this.googleDot.googleLinkUrl
+    //    },
+    //    parentDotId: this.parentDotId
+    //
+    //});
   }
 });
