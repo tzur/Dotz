ed309647ad07fbea4ad02469f35a7a7c8d1f8111
yoav <yoavsib@gmail.com>
2016-02-23 03:44:41 +0200

    monday before d-day 2

--- client/templates/public/landing pages/landingLogin.js
+++ client/templates/public/landing pages/landingLogin.js
@@ -8,20 +8,20 @@ Template.landingLogin.onDestroyed(function(){
 
 Template.landingLogin.helpers({
 
-  landingLoginTitle: function() {
+  landingTitle: function() {
     if ( Session.get('iAmAHotel') ) {
       return ("MAKE YOUR GUESTS HAPPIER WITH DOTZ");
     } else {
-      return ("Dotz. Connect The City");
+      return ("A Discovery Tool for Communities");
     }
   },
 
-  landingLoginSubTitle: function() {
+  landingSubTitle: function() {
     if ( Session.get('iAmAHotel') ) {
       return Spacebars.SafeString(
         "Connect the city Events, Places and Things-To-Do in custom-made lists." + '</h5><h5>' + "Share it with your guests.");
     } else {
-      return ("Connect the city Events, Places and Things-To-Do in custom-made lists.");
+      return ("With Dotz, community members can collaborate and create an outstanding knowledge base.");
     }
   }
 
--- client/templates/public/landing pages/landingSignup.js
+++ client/templates/public/landing pages/landingSignup.js
@@ -22,7 +22,7 @@ Template.landingSignup.helpers({
     if ( Session.get('iAmAHotel') ) {
       return ("MAKE YOUR GUESTS HAPPIER WITH DOTZ");
     } else {
-      return ("Dotz. Connect The City");
+      return ("A Discovery Tool for Communities");
     }
   },
 
@@ -31,7 +31,7 @@ Template.landingSignup.helpers({
       return Spacebars.SafeString(
         "Connect the city Events, Places and Things-To-Do in custom-made lists." + '</h5><h5>' + "Share it with your guests.");
     } else {
-      return ("Connect the city Events, Places and Things-To-Do in custom-made lists.");
+      return ("With Dotz, community members can collaborate and create an outstanding knowledge base.");
     }
   }
 
