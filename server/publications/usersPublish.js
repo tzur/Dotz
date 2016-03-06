3411878fca001b14aad8ef5171c55ee789b8eff6
otniel <otniel.levi@gmail.com>
2016-03-06 16:20:20 +0200

    pub-sub, userShow, broken links and more + lastDotzUserConnectedTo, connectDot, ..

--- server/publications/usersPublish.js
+++ server/publications/usersPublish.js
@@ -16,47 +16,52 @@ Meteor.publish( 'userByUserSlug1-OLD', function( userSlug ) {
 Meteor.publish('userByUserSlug', function(userSlug){
   check(userSlug, String);
   let user = Meteor.users.findOne({"profile.userSlug": userSlug});
-  let data = [
-    Meteor.users.find({"profile.userSlug": userSlug},
-      {fields: {
-        "username": 1,
-        "profile.userSlug": 1,
-        "profile.description": 1,
-        "profile.profileImage": 1,
-        "profile.coverImage": 1,
-        "profile.userType": 1,
-        "profile.shareDotId": 1, //TBD..
-        "profile.profileDotId": 1,
-        "profile.createdByUserLists": 1,
-        "profile.location": 1,
-        "profile.userAddress": 1,
-        "profile.websiteUrl": 1,
-        "profile.facebookAccountUrl": 1,
-        "profile.twitterAccountUrl": 1,
-        "profile.googleAccountUrl": 1}
-    }),
-
-    Dotz.find({_id: { $in: [user.profile.profileDotId, user.profile.shareDotId] }}, //TBD
+  let data;
+  if (user) {
+    data = [
+      Meteor.users.find({"profile.userSlug": userSlug},
         {fields: {
-        "isOpen": 1,
-        "ownerUserId": 1,
-        "connectedDotzArray": 1}
-    }),
+          "username": 1,
+          "profile.userSlug": 1,
+          "profile.description": 1,
+          "profile.profileImage": 1,
+          "profile.coverImage": 1,
+          "profile.userType": 1,
+          "profile.shareDotId": 1, //TBD..
+          "profile.profileDotId": 1,
+          "profile.createdByUserLists": 1,
+          "profile.location": 1,
+          "profile.userAddress": 1,
+          "profile.websiteUrl": 1,
+          "profile.facebookAccountUrl": 1,
+          "profile.twitterAccountUrl": 1,
+          "profile.googleAccountUrl": 1}
+        }),
 
-    UserConnections.find({userId: user._id} //TBD: right now we are actually publishing the full Doc :(
-      //,
-      //{fields: {
-      //  userId: 1,
-      //  createdByUserDotz: 1, //TBD
-      //  likesMadeByUserCounter: 1,
-      //  connectionsMadeByUserCounter:1,
-      //  peopleConnectedMyDotzCounter: 1,
-      //  peopleLikedMyConnectionsCounter:1,
-      //  peopleLikedMyDotzCounter: 1,
-      //  createdByUserDotzCounter: 1}
-      //}
-     )
-  ];
+      Dotz.find({_id: { $in: [user.profile.profileDotId, user.profile.shareDotId] }}, //TBD
+        {fields: {
+          "isOpen": 1,
+          "ownerUserId": 1,
+          "connectedDotzArray": 1}
+        }),
+
+      UserConnections.find({userId: user._id} //TBD: right now we are actually publishing the full Doc :(
+        //,
+        //{fields: {
+        //  userId: 1,
+        //  createdByUserDotz: 1, //TBD
+        //  likesMadeByUserCounter: 1,
+        //  connectionsMadeByUserCounter:1,
+        //  peopleConnectedMyDotzCounter: 1,
+        //  peopleLikedMyConnectionsCounter:1,
+        //  peopleLikedMyDotzCounter: 1,
+        //  createdByUserDotzCounter: 1}
+        //}
+      )
+    ];
+  }
+
+
   if ( data ) {
     return data;
   }
